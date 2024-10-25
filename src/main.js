c5ae535743aa0257f2d7a90866c4a6a490c842bd
Vadim <zankov.vadik@yandex.ru>
2024-10-25 14:32:10 +0500

    change phone

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from "./router/router";
 import axios from "axios";
 // при пустом url запросы автоматически идут на исходный сервер,
 // поэтому коренной рут по сути не нужен
-axios.defaults.baseURL = "http://62.113.107.148:3000";
+axios.defaults.baseURL = "http://62.113.107.148:3000/";
 
 const app = createApp(App);
 
